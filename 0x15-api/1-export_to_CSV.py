b3e660d9a6d742ebf0f8a0af1319b79eb134ac9a
laban254 <labanrotich6544@gmail.com>
2023-04-24 19:36:47 +0300

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""export data in the CSV format in an API"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    employeeId = argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
